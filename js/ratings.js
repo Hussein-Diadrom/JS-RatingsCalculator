26085b1658882c7b2e77b833b27a72ea9a636377
Hussein-Diadrom <95290186+Hussein-Diadrom@users.noreply.github.com>
2021-12-06 15:16:00 +0100

    tast6

--- js/ratings.js
+++ js/ratings.js
@@ -2,6 +2,8 @@ function collect_ratings() {
     const ratings = { count: 0, sum: 0, averge: 0 };
 
     let rating = 0; 
+    const elements = document.querySelectorAll(".rating");
+
+    elements.forEach(element => { });
 }
 
-const elements = document.querySelectorAll(".rating");
